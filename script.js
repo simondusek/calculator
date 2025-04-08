dba5636a5026f52462cf056a442297d2181c4a46
simondusek <dusek.simon@icloud.com>
2025-04-08 17:29:03 +0200

    Commit before rebuilding the code + adding .gitignore file.

--- script.js
+++ script.js
@@ -1,86 +1,80 @@
-let firstNum
-let secondNum
-let operator
+let firstNum;
+let secondNum;
+let operator = ["+", "-", "=", "del", "C", "AC"];
+let mainLine = document.getElementById("display");
+let subLine = document.getElementById("subsection");
 
-document.getElementById('1').addEventListener('click', function(){
-    document.getElementById('display').textContent += 1
+document.addEventListener("click", function (event) {
+    console.log(mainLine.textContent);
+    console.log(event.target.textContent);
+    if (event.target.nodeName === "BUTTON") {
+        if (!operator.includes(event.target.textContent)) {
+            mainLine.textContent += event.target.textContent;
+        } else if (event.target.textContent === "del") {
+            mainLine.textContent = mainLine.textContent.slice(0, mainLine.textContent.length - 1);
+        } else if (event.target.textContent === "C") {
+            activeOperator = operator[4];
+            mainLine.textContent = 0;
+        } else if (event.target.textContent === "AC") {
+            mainLine.textContent = "";
+            subLine.textContent = "";
+            firstNum = undefined;
+            secondNum = undefined;
+        } /*else {
+        if (event.target.textContent === operator[0]){
+            add();
+        } else if (event.target.textContent === operator[1]){
+            subtract();
+        } else {
+            result();
+        }
+    }*/
     }
-);
-document.getElementById('2').addEventListener('click', function(){
-    document.getElementById('display').textContent += 2
+});
+
+document.getElementById("+").addEventListener("click", function () {
+    firstNum = Number(mainLine.textContent);
+    subLine.textContent = `${firstNum} +`;
+    mainLine.textContent = "";
+    activeOperator = operator[0];
+    console.log("firstNum = " + firstNum);
+    console.log("secondNum = " + secondNum);
+});
+document.getElementById("-").addEventListener("click", function () {
+    firstNum = Number(mainLine.textContent);
+    subLine.textContent = `${firstNum} -`;
+    mainLine.textContent = "";
+    activeOperator = operator[1];
+    console.log("firstNum = " + firstNum);
+    console.log("secondNum = " + secondNum);
+});
+document.getElementById("=").addEventListener("click", function () {
+    if (secondNum == undefined) {
+        secondNum = Number(mainLine.textContent);
     }
-);
-document.getElementById('3').addEventListener('click', function(){
-    document.getElementById('display').textContent += 3
+    else {
+        firstNum = Number(mainLine.textContent)
     }
-);
-document.getElementById('4').addEventListener('click', function(){
-    document.getElementById('display').textContent += 4
-    }
-);
-document.getElementById('5').addEventListener('click', function(){
-    document.getElementById('display').textContent += 5
-    }
-);
-document.getElementById('6').addEventListener('click', function(){
-    document.getElementById('display').textContent += 6
-    }
-);
-document.getElementById('7').addEventListener('click', function(){
-    document.getElementById('display').textContent += 7
-    }
-);
-document.getElementById('8').addEventListener('click', function(){
-    document.getElementById('display').textContent += 8
-    }
-);
-document.getElementById('9').addEventListener('click', function(){
-    document.getElementById('display').textContent += 9
-    }
-);
-document.getElementById('+').addEventListener('click', function(){
-    firstNum = Number(document.getElementById('display').textContent)
-    console.log(document.getElementById('subsection').textContent = `${firstNum} +`)
-    //document.getElementById('subsection').textContent = `${firstNum} +`
-    document.getElementById('display').textContent = ''
-    operator = '+'
-    }
-);
-document.getElementById('-').addEventListener('click', function(){
-    firstNum = Number(document.getElementById('display').textContent)
-    console.log(document.getElementById('subsection').textContent = `${firstNum} -`)
-    //document.getElementById('subsection').textContent = `${firstNum} +`
-    document.getElementById('display').textContent = ''
-    operator = '-'
-    }
-);
-document.getElementById('=').addEventListener('click', function(){
-    secondNum = Number(document.getElementById('display').textContent)
-    if (operator == '+'){
-    document.getElementById('display').textContent = firstNum + Number(document.getElementById('display').textContent)
-    document.getElementById('subsection').textContent = `${firstNum} + ${secondNum} = `
+    console.log("firstNum = " + firstNum);
+    console.log("secondNum = " + secondNum);
+    if (activeOperator == operator[0]) {
+        mainLine.textContent = add(firstNum, secondNum);
+        subLine.textContent = `${firstNum} + ${secondNum} = `;
     } else {
-        document.getElementById('display').textContent = firstNum - Number(document.getElementById('display').textContent)
-        document.getElementById('subsection').textContent = `${firstNum} - ${secondNum} = `
+        mainLine.textContent = subtract(firstNum - Number(mainLine.textContent));
+        subLine.textContent = `${firstNum} - ${secondNum} = `;
+        // firstNum = mainLine
+        // secondNum = zustane to, cim bylo
     }
-
-    
-    }
-    
+});
+document.addEventListener("click", () =>
+    console.log(typeof mainLine.textContent)
 );
 
-document.addEventListener('click', () => console.log(typeof(document.getElementById('display').textContent)))
+function add() {
+    return firstNum + secondNum;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function subtract() {
+    return firstNum - secondNum;
+}
