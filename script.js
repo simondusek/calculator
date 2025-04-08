02c0f5d2c0ce6e254da00e82660938de2864bf8b
simondusek <dusek.simon@icloud.com>
2025-04-08 17:30:24 +0200

    Rebuilded calculator. Edited html to be redesigned after. JS functionality optimized.

--- script.js
+++ script.js
@@ -1,80 +1,75 @@
-let firstNum;
-let secondNum;
-let operator = ["+", "-", "=", "del", "C", "AC"];
-let mainLine = document.getElementById("display");
-let subLine = document.getElementById("subsection");
+let expression1;
+let expression2;
+const operators = ["+", "-", "D", "C", "W"];
+const equal = ['=', 'Enter']
+let activeOperator;
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const line = document.getElementById('line');
+const log = document.getElementById('log');
 
-document.addEventListener("click", function (event) {
-    console.log(mainLine.textContent);
-    console.log(event.target.textContent);
-    if (event.target.nodeName === "BUTTON") {
-        if (!operator.includes(event.target.textContent)) {
-            mainLine.textContent += event.target.textContent;
-        } else if (event.target.textContent === "del") {
-            mainLine.textContent = mainLine.textContent.slice(0, mainLine.textContent.length - 1);
-        } else if (event.target.textContent === "C") {
-            activeOperator = operator[4];
-            mainLine.textContent = 0;
-        } else if (event.target.textContent === "AC") {
-            mainLine.textContent = "";
-            subLine.textContent = "";
-            firstNum = undefined;
-            secondNum = undefined;
-        } /*else {
-        if (event.target.textContent === operator[0]){
-            add();
-        } else if (event.target.textContent === operator[1]){
-            subtract();
-        } else {
-            result();
+document.addEventListener("keydown", function (event) {
+    console.log(`expression1 = ${expression1}, expression2 = ${expression2}, activeOperator = ${activeOperator}`)
+    if (numbers.includes(event.key)) {
+        line.textContent += event.key
+    }
+    if (event.key === 'Backspace') {
+        line.textContent = line.textContent.slice(0, (line.textContent.length - 1))
+    }
+
+
+
+
+
+    if (operators.includes(event.key)) {
+        if (activeOperator == undefined) { 
+            activeOperator = event.key;
+            expression1 = Number(line.textContent);
+            line.textContent = "";
+            log.textContent = (`${expression1} ${activeOperator}`);
+        } else { 
+            if (expression2 == undefined) {
+                expression2 = Number(line.textContent);
+            } else {
+                expression1 = getResult(expression1, expression2, activeOperator);
+            }
+            activeOperator = event.key;
+            expression2 = undefined; // Reset expression2 for the next input
+            line.textContent = "";
+            log.textContent = (`${expression1} ${activeOperator}`);
         }
-    }*/
+    };
+
+
+
+
+
+    if (equal.includes(event.key)) {
+        if (expression2 == undefined) {
+            expression2 = Number(line.textContent);
+        } else {
+            expression1 = getResult(expression1, expression2, activeOperator)
+        }
+
+        console.log(getResult(expression1, expression2, activeOperator));
+        line.textContent = getResult(expression1, expression2, activeOperator);
+        log.textContent = (`${expression1} ${activeOperator} ${expression2} ${equal[0]}`)
     }
 });
 
-document.getElementById("+").addEventListener("click", function () {
-    firstNum = Number(mainLine.textContent);
-    subLine.textContent = `${firstNum} +`;
-    mainLine.textContent = "";
-    activeOperator = operator[0];
-    console.log("firstNum = " + firstNum);
-    console.log("secondNum = " + secondNum);
-});
-document.getElementById("-").addEventListener("click", function () {
-    firstNum = Number(mainLine.textContent);
-    subLine.textContent = `${firstNum} -`;
-    mainLine.textContent = "";
-    activeOperator = operator[1];
-    console.log("firstNum = " + firstNum);
-    console.log("secondNum = " + secondNum);
-});
-document.getElementById("=").addEventListener("click", function () {
-    if (secondNum == undefined) {
-        secondNum = Number(mainLine.textContent);
-    }
-    else {
-        firstNum = Number(mainLine.textContent)
-    }
-    console.log("firstNum = " + firstNum);
-    console.log("secondNum = " + secondNum);
-    if (activeOperator == operator[0]) {
-        mainLine.textContent = add(firstNum, secondNum);
-        subLine.textContent = `${firstNum} + ${secondNum} = `;
-    } else {
-        mainLine.textContent = subtract(firstNum - Number(mainLine.textContent));
-        subLine.textContent = `${firstNum} - ${secondNum} = `;
-        // firstNum = mainLine
-        // secondNum = zustane to, cim bylo
-    }
-});
-document.addEventListener("click", () =>
-    console.log(typeof mainLine.textContent)
-);
 
-function add() {
-    return firstNum + secondNum;
+function getResult(exp1, exp2, operator) {
+    if (operator == '+') {
+        return add(exp1, exp2)
+    } else if (operator == '-') {
+        return sub(exp1, exp2)
+    }
 }
 
-function subtract() {
-    return firstNum - secondNum;
+function add(exp1, exp2) {
+    return (exp1 + exp2);
 }
+
+function sub(exp1, exp2) {
+    return (exp1 - exp2);
+}
+
